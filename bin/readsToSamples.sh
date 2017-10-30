302710370baf49451224e1aeccb03ee5c5a7d1cc
Steve Goldstein <sgoldstein@wisc.edu>
2017-10-30 12:25:15 -0500

    Testing and fixing

--- bin/readsToSamples.sh
+++ bin/readsToSamples.sh
@@ -15,8 +15,13 @@ tar zxf python277.withCutadapt.tar.gz
 rm python277.withCutadapt.tar.gz
 
 ### Envar needed for cutadapt
-export PATH=$(pwd)/python277/bin
+export PATH=$(pwd)/python277/bin:$PATH:.
 ######################################
+cluster=$1
+shift
+
+process=$1
+shift
 
 sampleFQ=$1
 shift
@@ -27,12 +32,14 @@ mkdir -p results/processedReads
 
 cp $sampleFQ .
 sampleFQ=$(basename $sampleFQ)
-resultsTarFile=${sampleFQ/\.fastq.gz/.tgz}
+resultsTarFile=${sampleFQ/.$cluster.$process.fastq.gz/.tgz}
 
 gunzip -c $sampleFQ > reads.fastq
+rm $sampleFQ
 
 nohup ./cutadapt -a GCWGAGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG -O 4 -o results/cutadaptOut.fastq reads.fastq > results/cutadapt.log
 
+rm reads.fastq
 
 INPUT=results/cutadaptOut.fastq
 for i in 8 7 6 5 4;
